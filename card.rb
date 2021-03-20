0f289f495a11709e4c1d70371d286004fe935894
VladimirShinkevich <78034441+VladimirShinkevich@users.noreply.github.com>
2021-03-20 16:43:44 +0300

    Update card.rb

--- card.rb
+++ card.rb
@@ -1,24 +1,7 @@
 # frozen_string_literal: true
 
 class Card
-  def initialize
-    @pack = []
-    create_pack
-  end
-
-  def give_card
-    take_card = @pack[rand(@pack.size)]
-    @pack.delete(take_card)
-  end
-
-  def create_pack
-    card_name = %w[A K Q J 10 9 8 7 6 5 4 3 2]
-    card_suit = %w[+ <3 ^ <>]
-    card_name.each do |name|
-      @pack << "#{name} #{card_suit[0]}"
-      @pack << "#{name} #{card_suit[1]}"
-      @pack << "#{name} #{card_suit[2]}"
-      @pack << "#{name} #{card_suit[3]}"
-    end
-  end
+  VOLUE = %w[A K Q J 10 9 8 7 6 5 4 3 2].freeze
+  SUIT = %w[+ <3 ^ <>].freeze
 end
+
