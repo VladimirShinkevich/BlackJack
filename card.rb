0e96c4f645573d1cef64bb9475c4eef9558db9fb
VladimirShinkevich <78034441+VladimirShinkevich@users.noreply.github.com>
2021-03-22 21:49:07 +0300

    Update card.rb

--- card.rb
+++ card.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 
 class Card
-  VOLUE = %w[A K Q J 10 9 8 7 6 5 4 3 2].freeze
-  SUIT = %w[+ <3 ^ <>].freeze
-end
+  attr_accessor :value, :suit
 
+  VALUE = %w[A K Q J 10 9 8 7 6 5 4 3 2].freeze
+  SUIT = %w[+ <3 ^ <>].freeze
+
+  def initialize(value, suit)
+    @suit = suit
+    @value = value
+  end
+
+  def to_s
+    "#{value} #{suit}"
+  end
+end
